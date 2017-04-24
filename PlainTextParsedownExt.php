4898deca4dbf218784ae70abadb687aeda9f3576
Aidan Woods <aidantwoods@gmail.com>
2017-04-24 12:04:16 +0100

    add plain text conversion extension

---
+++ PlainTextParsedownExt.php
@@ -0,0 +1,27 @@
+<?php
+
+class PlainTextParsedownExt extends Parsedown
+{
+    protected function element(array $Element)
+    {
+        $markup = '';
+
+        if (isset($Element['text']))
+        {
+            if (isset($Element['handler']))
+            {
+                $markup .= $this->{$Element['handler']}($Element['text']);
+            }
+            else
+            {
+                $markup .= $Element['text'];
+            }
+        }
+        elseif (isset($Element['attributes']['alt']))
+        {
+            $markup .= $Element['attributes']['alt'];
+        }
+
+        return $markup;
+    }
+}
